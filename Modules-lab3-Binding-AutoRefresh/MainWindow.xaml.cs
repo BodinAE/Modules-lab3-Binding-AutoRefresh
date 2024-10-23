a906be6be972be17778b6c93b1b0f2f20830695b
BodinAE <BodinAE@mer.ci.nsu.ru>
2024-10-23 12:43:05 +0700

    Add RemComm, Add janky AddComm

--- Modules-lab3-Binding-AutoRefresh/MainWindow.xaml.cs
+++ Modules-lab3-Binding-AutoRefresh/MainWindow.xaml.cs
@@ -22,72 +22,47 @@ namespace Modules_lab3_Binding_AutoRefresh
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-        public ObservableCollection<Mushroom> mushrooms { get; set; }
-        public Mushroom ioMush {
-            get
-            {
-                return iomush;
-            }
-            set
-            {
-                iomush = value;
-                OnPropertyChanged();
-            }
-        }
-
-        private Mushroom iomush;
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void OnPropertyChanged([CallerMemberName] string prop = "")
-        {
-            if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(prop));
-        }
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = this;
+            DataContext = new ViewModel();
+            //this.DataContext = this;
 
-            mushrooms = new ObservableCollection<Mushroom>()
-            {
-                new Mushroom(1, "Name1", "Colour1", true, 24.5, 32.4, 86.3),
-                new Mushroom(2, "Name2", "Colour2", true, 24.5, 32.4, 86.3),
-                new Mushroom(3, "Name3", "Colour3", true, 24.5, 32.4, 86.3)
-            };
-        }
-
-        private void RemoveButton_Click(object sender, RoutedEventArgs e)
-        {
-            mushrooms.RemoveAt(LV_Mushrooms.SelectedIndex);
-        }
-
-        private void AddButton_Click(object sender, RoutedEventArgs e)
-        {
-            //int.TryParse(TB_ID.Text, out int id);
-            //string name = TB_Name.Text;
-            //string colour = TB_Colour.Text;
-            //bool edibility = Convert.ToBoolean(ChB_Edibility.IsChecked);
-            //double.TryParse(TB_Weight.Text, System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture, out double weight);
-            //double.TryParse(TB_Height.Text, System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture, out double height);
-            //double.TryParse(TB_Cap_Radius.Text, System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture, out double cap_radius);
-            //mushrooms.Add(new Mushroom(id, name, colour, edibility, weight, height, cap_radius));
-            mushrooms.Add(new Mushroom(ioMush.ID, ioMush.Name, ioMush.Colour, ioMush.Edibility, ioMush.Weight, ioMush.Height, ioMush.Cap_Radius));
-        }
-
-        private void LV_Mushrooms_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            ListView view = (ListView)sender;
-            ioMush = view.SelectedItem as Mushroom;
-            //ioMush.ID = mushrooms[LV_Mushrooms.SelectedIndex].ID;
-            //ioMush.Name = mushrooms[LV_Mushrooms.SelectedIndex].Name;
-            //ioMush.Colour = mushrooms[LV_Mushrooms.SelectedIndex].Colour;
-            //ioMush.Edibility = mushrooms[LV_Mushrooms.SelectedIndex].Edibility;
-            //ioMush.Weight = mushrooms[LV_Mushrooms.SelectedIndex].Weight;
-            //ioMush.Height = mushrooms[LV_Mushrooms.SelectedIndex].Height;
-            //ioMush.Cap_Radius = mushrooms[LV_Mushrooms.SelectedIndex].Cap_Radius;
             
         }
+
+        //private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    mushrooms.RemoveAt(LV_Mushrooms.SelectedIndex);
+        //}
+
+        //private void AddButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    //int.TryParse(TB_ID.Text, out int id);
+        //    //string name = TB_Name.Text;
+        //    //string colour = TB_Colour.Text;
+        //    //bool edibility = Convert.ToBoolean(ChB_Edibility.IsChecked);
+        //    //double.TryParse(TB_Weight.Text, System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture, out double weight);
+        //    //double.TryParse(TB_Height.Text, System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture, out double height);
+        //    //double.TryParse(TB_Cap_Radius.Text, System.Globalization.NumberStyles.Any, CultureInfo.InvariantCulture, out double cap_radius);
+        //    //mushrooms.Add(new Mushroom(id, name, colour, edibility, weight, height, cap_radius));
+        //    mushrooms.Add(new Mushroom(ioMush.ID, ioMush.Name, ioMush.Colour, ioMush.Edibility, ioMush.Weight, ioMush.Height, ioMush.Cap_Radius));
+        //}
+
+        //private void LV_Mushrooms_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    ListView view = (ListView)sender;
+        //    ioMush = view.SelectedItem as Mushroom;
+        //    //ioMush.ID = mushrooms[LV_Mushrooms.SelectedIndex].ID;
+        //    //ioMush.Name = mushrooms[LV_Mushrooms.SelectedIndex].Name;
+        //    //ioMush.Colour = mushrooms[LV_Mushrooms.SelectedIndex].Colour;
+        //    //ioMush.Edibility = mushrooms[LV_Mushrooms.SelectedIndex].Edibility;
+        //    //ioMush.Weight = mushrooms[LV_Mushrooms.SelectedIndex].Weight;
+        //    //ioMush.Height = mushrooms[LV_Mushrooms.SelectedIndex].Height;
+        //    //ioMush.Cap_Radius = mushrooms[LV_Mushrooms.SelectedIndex].Cap_Radius;
+            
+        //}
     }
 }
---
+++ Modules-lab3-Binding-AutoRefresh/ViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modules_lab3_Binding_AutoRefresh
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        private RelayCommand addComm;
+        public RelayCommand AddComm 
+        {  
+            get
+            {
+                return addComm ?? (addComm = new RelayCommand(obj => 
+                {
+                    var mush = new Mushroom(0, "New Mushroom", "Colour", false, 0, 0, 0);
+                    mushrooms.Add(mush);
+                }));
+            } 
+        }
+        private RelayCommand remComm;
+        public RelayCommand RemComm
+        {
+            get
+            {
+                return remComm ?? (remComm = new RelayCommand(obj =>
+                {
+                    var mush = obj as Mushroom;
+                    mushrooms.Remove(mush);
+                }));
+            }
+        }
+
+        private Mushroom selectedMushroom;
+        public Mushroom SelectedMushroom 
+        { 
+            get
+            {
+                return selectedMushroom;
+            }
+            set
+            {
+                selectedMushroom = value;
+                OnPropertyChanged("SelectedMushroom");
+            }
+        }
+
+        public ViewModel()
+        {
+            mushrooms = new ObservableCollection<Mushroom>()
+            {
+                new Mushroom(1, "Name1", "Colour1", true, 24.5, 32.4, 86.3),
+                new Mushroom(2, "Name2", "Colour2", true, 24.5, 32.4, 86.3),
+                new Mushroom(3, "Name3", "Colour3", true, 24.5, 32.4, 86.3)
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+
+        }
+        public ObservableCollection<Mushroom> mushrooms { get; set; }
+    }
+}
